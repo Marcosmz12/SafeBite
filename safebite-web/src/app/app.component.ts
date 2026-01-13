a777154083d3583cb3462f5517743696f26692b4
Marcosmz12 <mmolzap0412@g.educaand.es>
2026-01-13 18:07:40 +0100

    Cambios en la web , componentes creados para el header , footer y hero que seria el body

--- safebite-web/src/app/app.component.ts
+++ safebite-web/src/app/app.component.ts
@@ -2,11 +2,14 @@ import { Component, inject } from '@angular/core';
 import { CommonModule, AsyncPipe } from '@angular/common';
 import { RecetasService } from './services/recetas.service';
 import { Observable } from 'rxjs';
+import { HeaderComponent } from "./components/header/header.component";
+import { HeroComponent } from "./components/hero/hero.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, AsyncPipe],
+  imports: [CommonModule, HeaderComponent, HeroComponent, FooterComponent],
   templateUrl: './app.component.html',  // <-- Ahora apunta al archivo externo
   styleUrl: './app.component.css'       // <-- También para los estilos
 })
