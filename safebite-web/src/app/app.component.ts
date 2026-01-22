d85bdee4137ead5818c54b2d8a30a3870b47369e
Marcosmz12 <mmolzap0412@g.educaand.es>
2026-01-22 20:21:44 +0100

    haciendo la web responsiva

--- safebite-web/src/app/app.component.ts
+++ safebite-web/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { ChatbotComponent } from "./components/chatbot/chatbot.component";
+import { ChatbotComponent } from './components/chatbot/chatbot.component';
 
 @Component({
   selector: 'app-root',
