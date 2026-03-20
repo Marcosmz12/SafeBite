606337671eaf12257dd996e2826b45fd07aa02da
jmm96-13 <jmm.cv.96@gmail.com>
2026-03-20 11:14:09 +0100

    visual subir receta

--- safebite-web/src/app/app.config.ts
+++ safebite-web/src/app/app.config.ts
@@ -30,7 +30,7 @@ export const appConfig: ApplicationConfig = {
     {
       provide: RECAPTCHA_SETTINGS,
       useValue: {
-        siteKey: '6LffT1ksAAAAAG_jetn_8kPAhVa5PhR4rKCURtaQ', // <-- PEGA AQUÍ TU CLAVE DE SITIO
+        siteKey: '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI', // <-- PEGA AQUÍ TU CLAVE DE SITIO
       } as RecaptchaSettings,
     },
   ]
